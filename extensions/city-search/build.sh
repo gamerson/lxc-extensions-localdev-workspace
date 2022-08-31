99cd984450c3206feefe8248af3e9530181cfd8e
Raymond Augé <raymond.auge@liferay.com>
2022-08-31 14:51:12 -0400

    city-search build to deploy direct to dxp docker

--- extensions/city-search/build.sh
+++ extensions/city-search/build.sh
@@ -4,10 +4,6 @@ set -e
 
 cd "$(dirname "$0")"
 
-if [[ ! -z "${EXPECTED_REF}" ]]; then
-  IMAGE=${EXPECTED_REF}
-fi
-
 ../../gradlew :extensions:city-search:clean
 
 yarn install && yarn build-local
@@ -16,8 +12,6 @@ yarn install && yarn build-local
 
 cd dist
 
-unzip -o city-search.zip
+DEPLOY=../../../build/docker/client-extensions/
 
-docker build -t $IMAGE .
-
-tilt trigger "(Tiltfile)"
+cp -f city-search.zip "${DEPLOY}city-search.zip"
