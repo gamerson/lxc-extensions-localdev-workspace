9c771f1be8211b4782ab9f10a39879298e2225b0
Raymond Augé <raymond.auge@liferay.com>
2022-09-14 16:17:35 -0400

    add the action endpoint

--- extensions/couponpdf/src/main/java/com/liferay/couponpdf/service/CouponsController.java
+++ extensions/couponpdf/src/main/java/com/liferay/couponpdf/service/CouponsController.java
@@ -26,6 +26,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -41,6 +43,16 @@ public class CouponsController {
   private String _mainDomain;
 
   @Autowired private WebClient _webClient;
+
+  @PostMapping(
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE,
+      value = "/issued")
+  public ResponseEntity<String> create(@RequestBody String couponJson) {
+    System.out.println("HERE IS THE OBJECT JSON: " + couponJson);
+
+    return new ResponseEntity<>("OK", HttpStatus.CREATED);
+  }
 
   @GetMapping(
       produces = MediaType.APPLICATION_PDF_VALUE,
